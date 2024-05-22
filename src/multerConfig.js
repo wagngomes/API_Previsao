8e3f9d4356e715e53e81b06b82d5e895944b0200
wagngomes <wagner.gomes.cx2@gmail.com>
2024-05-22 17:28:57 +0000

    estapa 1 , ainda tentando importar

--- src/multerConfig.js
+++ src/multerConfig.js
@@ -1,5 +1,7 @@
 const multer = require('multer')
 const path = require('path')
+const fs = require('fs')
+
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
--- src/routes/uploadRoute.js
+++ src/routes/uploadRoute.js
@@ -3,6 +3,8 @@ const multer = require('multer')
 const { PrismaClient } = require('@prisma/client')
 const { Router } = require('express')
 const express = require('express')
+const fs = require('fs');
+const csv = require('csv-parse');
 
 const upload = multer({storage: storage})
 const prisma = new PrismaClient
@@ -23,28 +25,28 @@ router.post('/upload', upload.single('file') ,(req, res) => {
             codigo: data.codigo,
             descricao: data.descricao,
             fornecedor: data.fornecedor,
-            qtd_pedido: data.qtd_pedido,
-            embalagem_padrao: data.embalagem_padrao,
-            qtd_corrigida: data.qtd_corrigida,
+            qtd_pedido: parseInt(data.qtd_pedido),
+            embalagem_padrao: parseInt(data.embalagem_padrao),
+            qtd_corrigida: parseInt(data.qtd_corrigida),
             data_colocacao: data.data_colocacao,
             semana_compra: data.semana_compra,
             rota_cod: data.rota_cod,
             centro: data.centro,
             rota: data.rota,
-            preco_unitario: data.preco_unitario,
-            valor_total: data.valor_total,
+            preco_unitario: parseFloat(data.preco_unitario),
+            valor_total: parseFloat(data.valor_total),
             dia_colocacao: data.dia_colocacao,
             comprador: data.comprador,
-            de_chao: data.de_chao,
-            de_simulado: data.de_simulado,
-            de_fechamento_sim_viveo: data.de_fechamento_sim_viveo,
-            politica: data.politica,
-            forecast: data.forecast,
-            estoque_chao: data.estoque_chao,
-            estoque_total: data.estoque_total,
-            delta_forecast: data.delta_forecast,
-            estoque_projetado_c_pedido: data.estoque_projetado_c_pedido,
-            de_com_pedido_filial: data.de_com_pedido_filial
+            de_chao: parseFloat(data.de_chao),
+            de_simulado: parseFloat(data.de_simulado),
+            de_fechamento_sim_viveo: parseFloat(data.de_fechamento_sim_viveo),
+            politica: parseInt(data.politica),
+            forecast: parseInt(data.forecast),
+            estoque_chao: parseInt(data.estoque_chao),
+            estoque_total: parseInt(data.estoque_total),
+            delta_forecast: parseInt(data.delta_forecast),
+            estoque_projetado_c_pedido: parseInt(data.estoque_projetado_c_pedido),
+            de_com_pedido_filial: parseFloat(data.de_com_pedido_filial)
 
         };
 
